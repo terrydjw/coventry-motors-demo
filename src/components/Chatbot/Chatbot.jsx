cb66fec8dbae53f01a226e4d0106adbad2ea92bf
unknown <dylanwilliams49@live.co.uk>
2025-09-13 14:45:18 +0100

    demo commit

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -24,7 +24,7 @@ const Chatbot = () => {
     useEffect(() => {
         if (isOpen && messages.length === 0) {
             setIsTyping(true);
-            fetch('http://127.0.0.1:5000/chat', {
+            fetch('https://api.verndigital.com/chat', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ message: '' }),
@@ -53,7 +53,7 @@ const Chatbot = () => {
         setShowQuickReplies(false);
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/chat', {
+            const response = await fetch('https://api.verndigital.com/chat', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ message: messageText }),
